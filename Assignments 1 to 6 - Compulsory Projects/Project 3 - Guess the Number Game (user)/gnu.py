1aff3934561eb14122f80694f422337067637241
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-05 01:33:58 +0500

    Guess the Number (User) (v0.2)

--- Assignments 1 to 6 - Compulsory Projects/Project 3 - Guess the Number Game (user)/gnu.py
+++ Assignments 1 to 6 - Compulsory Projects/Project 3 - Guess the Number Game (user)/gnu.py
@@ -1,1 +1,42 @@
-print("Code in progress...")
+# Guess The Number (User) - by mshsheikh
+
+import random
+
+print("\n-- -- -- -- -- -- -- -- -- -- -- --")
+print("          GUESS THE NUMBER         ")
+print("  Play with computer and have fun! ")
+print("-- -- -- -- -- -- -- -- -- -- -- --\n")
+
+print("""User 👤: I have a number between 1 to 6.
+         Let's see if the computer can guess it!""")
+
+low = 1
+high = 6
+
+while True:
+  computer_guess = random.randint(low, high)
+  print(f"\nBOT 🤖: Is it {computer_guess}?")
+
+  feedback = input("\nEnter L if it's too Low, H if it's too High, or C if it's Correct: ").lower()
+
+  if feedback == 'l':
+    low = computer_guess + 1
+    print("\nBOT 🤖: Hmm... Too low? Let me guess higher.")
+
+  elif feedback == 'h':
+    high = computer_guess - 1
+    print("\nBOT 🤖: Oh! Too high? I'll try a smaller one.")
+
+  elif feedback == 'c':
+    print("\nBOT 🤖: Yay! I guessed it right!")
+
+    break
+
+  else:
+    print("Please enter a valid option: L, H, or C.")
+
+footer_A = "\nMade with ❤️  just for you!"
+footer_B = "by Muhammad Salman Hussain\n"
+
+print(footer_A)
+print(footer_B)
