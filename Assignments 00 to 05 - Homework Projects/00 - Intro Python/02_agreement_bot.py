307aebc6e253ba624dc8c67e116c409616c8f991
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-07 18:17:15 +0500

    Agreement Bot (v0.1)

---
+++ Assignments 00 to 05 - Homework Projects/00 - Intro Python/02_agreement_bot.py
@@ -0,0 +1,13 @@
+print("\n-- -- -- -- -- -- --")
+print("  00 - INTRO PYTHON ")
+print("   Agreement Bot   ")
+print("-- -- -- -- -- -- --\n")
+
+print("Computer: What's your favorite animal?\n")
+
+user_animal : str = input("User: ")
+
+print(f"\nComputer: {user_animal} is also my favorite animal!")
+
+footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
