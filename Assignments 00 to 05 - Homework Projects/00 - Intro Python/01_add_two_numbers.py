3800bab4717b8b71862986e1d53653b815ddc2ae
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-07 17:56:34 +0500

    Add Two Numbers (v0.1)

---
+++ Assignments 00 to 05 - Homework Projects/00 - Intro Python/01_add_two_numbers.py
@@ -0,0 +1,14 @@
+print("\n-- -- -- -- -- -- --")
+print("  00 - INTRO PYTHON ")
+print("   Add Two Numbers   ")
+print("-- -- -- -- -- -- --\n")
+
+user_num1 : int = int(input("Enter your first number: "))
+user_num2 : int = int(input("Enter your second number: "))
+
+result = user_num1 + user_num2
+
+print(f"Addition of your provided numbers is: {result}")
+
+footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
