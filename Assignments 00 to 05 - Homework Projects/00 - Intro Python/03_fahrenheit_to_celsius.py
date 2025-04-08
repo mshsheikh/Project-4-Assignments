c30b8a47f96863f7a792594e929e648b71016823
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-08 18:19:54 +0500

    F2C (v0.1)

---
+++ Assignments 00 to 05 - Homework Projects/00 - Intro Python/03_fahrenheit_to_celsius.py
@@ -0,0 +1,13 @@
+print("\n-- -- -- -- -- -- -- -- -- -- -- --")
+print("         00 - INTRO PYTHON ")
+print("  Fahrenheit to Celsius Converter   ")
+print("-- -- -- -- -- -- -- -- -- -- -- --\n")
+
+F = float(input("Enter Tempreture in Fahrenheit: "))
+
+C = (F - 32) * 5.0/9.0
+
+print(f'\nOutput: {round(F)}°F is {round(C)}°C')
+
+footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
