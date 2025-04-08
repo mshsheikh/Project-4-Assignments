9640efaa860274053dbffa4b35934b97ec69306f
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-09 00:50:41 +0500

    Dice Simulator (v0.2)

--- Assignments 00 to 05 - Homework Projects/01 - Expressions/01_dicesimulator.py
+++ Assignments 00 to 05 - Homework Projects/01 - Expressions/01_dicesimulator.py
@@ -3,7 +3,25 @@ print("  01 - EXPRESSIONS ")
 print("   Dice Simulator   ")
 print("-- -- -- -- -- -- --\n")
 
+import random
 
+Die1 = random.randint(1, 6)
+print(f'Die 1 rolls, and we have {Die1} on top.\n')
+
+Die2 = random.randint(1, 6)
+print(f'Die 2 rolls, and we have {Die2} on top.\n')
+
+Die3 = random.randint(1, 6)
+print(f'Die 3 rolls, and we have {Die3} on top.\n')
+
+Die4 = random.randint(1, 6)
+print(f'Die 4 rolls, and we have {Die4} on top.\n')
+
+Die5 = random.randint(1, 6)
+print(f'Die 5 rolls, and we have {Die5} on top.\n')
+
+Die6 = random.randint(1, 6)
+print(f'Die 6 rolls, and we have {Die6} on top.\n')
 
 footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
 print(footer_A)
