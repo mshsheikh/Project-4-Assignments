ae6b6f0d2d7e3e72ae9a459f164e47fc71011c3f
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-08 20:54:09 +0500

    People Age (v0.1)

---
+++ Assignments 00 to 05 - Homework Projects/00 - Intro Python/04_how_old_are_they.py
@@ -0,0 +1,19 @@
+print("\n-- -- -- -- -- -- --")
+print("  00 - INTRO PYTHON ")
+print("  How Old They Are   ")
+print("-- -- -- -- -- -- --\n")
+
+Anthon = 21
+Beth  = Anthon +6
+Chen = Beth + 20
+Drew = Chen + Anthon
+Ethon = Chen
+
+print(f"Antho's age is {Anthon}.")
+print(f"Beth's age is {Beth}.")
+print(f"Chen's age is {Chen}.")
+print(f"Drew's age is {Drew}.")
+print(f"Ethan's age is {Ethon}.")
+
+footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
