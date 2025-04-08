3f3e303df49e1be52a5b107d7aa8c41f326bc585
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-08 21:09:20 +0500

    Square (v0.1)

---
+++ Assignments 00 to 05 - Homework Projects/00 - Intro Python/06_square_number.py
@@ -0,0 +1,13 @@
+print("\n-- -- -- -- -- -- --")
+print("  00 - INTRO PYTHON ")
+print("   Square  Number   ")
+print("-- -- -- -- -- -- --\n")
+
+user_num = int(input("Enter your required number to get square: "))
+num_sq = user_num ** 2
+
+print(f'''{user_num}'s square is {num_sq}.''')
+
+
+footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
