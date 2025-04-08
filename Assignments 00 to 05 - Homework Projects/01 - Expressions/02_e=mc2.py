df14db03f19f2d078a62c2aa49ccb449a8dd2dac
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-09 01:34:56 +0500

    e=mc2 (v0.2)

--- Assignments 00 to 05 - Homework Projects/01 - Expressions/02_e=mc2.py
+++ Assignments 00 to 05 - Homework Projects/01 - Expressions/02_e=mc2.py
@@ -3,7 +3,21 @@ print("  01 - EXPRESSIONS ")
 print("      e = mc²   ")
 print("-- -- -- -- -- -- --\n")
 
+print('''We're using Einstein's equation e = mc²\n''')
 
+c = 299_792_458 # Speed of light in m/s
+
+user_mass = float(input("Enter mass in Kilogram: "))
+
+m = user_mass
+
+E = m * c**2
+
+print(f"""
+e = energy in joules, which is {E}.
+m = mass in kilograms, which is {m}.
+c = speed of light in m/s, which is {c}.
+""")
 
 footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
 print(footer_A)
