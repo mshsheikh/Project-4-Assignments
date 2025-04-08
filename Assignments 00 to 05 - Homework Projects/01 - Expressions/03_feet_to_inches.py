195f9f40542a647d48a1a47ff28532a92cb42ad4
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-09 02:15:06 +0500

    feet-to-inches (v0.2)

--- Assignments 00 to 05 - Homework Projects/01 - Expressions/03_feet_to_inches.py
+++ Assignments 00 to 05 - Homework Projects/01 - Expressions/03_feet_to_inches.py
@@ -1,9 +1,17 @@
 print("\n-- -- -- -- -- -- --")
 print("  01 - EXPRESSIONS ")
-print("   Dice Simulator   ")
+print("   Feet to Inches  ")
 print("-- -- -- -- -- -- --\n")
 
+ft = float(input("Enter length in feet: "))
 
+inch = ft * 12
+
+if ft > 1:
+  print(f"\n{ft} feets is equal to {inch} inches.\n")
+
+else:
+  print(f"\n{ft} feet is equal to {inch} inch.\n")
 
 footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
 print(footer_A)
