c7ab015b67438cbf077966e2acce2dc12186ad50
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-09 00:26:43 +0500

    feet-to-inches (v0.1)

---
+++ Assignments 00 to 05 - Homework Projects/01 - Expressions/03_feet_to_inches.py
@@ -0,0 +1,9 @@
+print("\n-- -- -- -- -- -- --")
+print("  01 - EXPRESSIONS ")
+print("   Dice Simulator   ")
+print("-- -- -- -- -- -- --\n")
+
+
+
+footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
