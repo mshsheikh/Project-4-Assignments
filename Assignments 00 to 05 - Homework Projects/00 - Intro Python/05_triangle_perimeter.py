6c634844501c62a6ee5ebf62e93e0f595e29a0f5
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-08 20:55:06 +0500

    Triangle Size (v0.1)

---
+++ Assignments 00 to 05 - Homework Projects/00 - Intro Python/05_triangle_perimeter.py
@@ -0,0 +1,15 @@
+print("\n-- -- -- -- -- -- -- --")
+print("   00 - INTRO PYTHON ")
+print("  Triangle  Perimeter   ")
+print("-- -- -- -- -- -- -- --\n")
+
+angle1 = float(input("Enter first side length: "))
+angle2 = float(input("Enter second side length: "))
+angle3 = float(input("Enter third side length: "))
+
+total_length = angle1 + angle2 + angle3
+
+print(f"Total perimeter of the triangle is {total_length}.")
+
+footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
