6f5a899e712374c3d438a77d7cad0b03ede43b1c
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-09 00:25:45 +0500

    e=mc² (v0.1)

---
+++ Assignments 00 to 05 - Homework Projects/01 - Expressions/02_e=mc2.py
@@ -0,0 +1,9 @@
+print("\n-- -- -- -- -- -- --")
+print("  01 - EXPRESSIONS ")
+print("      e = mc²   ")
+print("-- -- -- -- -- -- --\n")
+
+
+
+footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
