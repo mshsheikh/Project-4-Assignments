631a94cea94169d2a3053882592f9dfbee99ef02
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-11 22:32:43 +0500

    Pythagorean Theorem

--- Assignments 00 to 05 - Homework Projects/01 - Expressions/04_pythagorean_theorem.py
+++ Assignments 00 to 05 - Homework Projects/01 - Expressions/04_pythagorean_theorem.py
@@ -1,9 +1,16 @@
+import math
+
 print("\n-- -- -- -- -- -- -- -- --")
 print("     01 - EXPRESSIONS ")
 print("   Pythagorean  Theorem   ")
 print("-- -- -- -- -- -- -- -- --\n")
 
+AB = float(input("Enter the length of AB: "))
+AC = float(input("Enter the length of AC: "))
 
+BC = math.sqrt(AB**2 + AC**2) # Hypotenuse
+
+print(f'\n"The length of BC (the hypotenuse) is: {BC}"')
 
 footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
 print(footer_A)
