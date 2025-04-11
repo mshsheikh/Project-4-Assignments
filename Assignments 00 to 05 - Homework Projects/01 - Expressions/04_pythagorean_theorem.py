6fc176a631277b2ebfcd3c851df7c54d63df8e8b
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-11 23:10:56 +0500

    Pythagorean Theorem (v0.3)

--- Assignments 00 to 05 - Homework Projects/01 - Expressions/04_pythagorean_theorem.py
+++ Assignments 00 to 05 - Homework Projects/01 - Expressions/04_pythagorean_theorem.py
@@ -8,6 +8,8 @@ print("-- -- -- -- -- -- -- -- --\n")
 AB = float(input("Enter the length of AB: "))
 AC = float(input("Enter the length of AC: "))
 
+# Hypotenuse² = AB² + AC²
+
 BC = math.sqrt(AB**2 + AC**2) # Hypotenuse
 
 print(f'\n"The length of BC (the hypotenuse) is: {BC}"')
