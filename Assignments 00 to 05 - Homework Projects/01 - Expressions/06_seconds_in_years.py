df76c0d241e6c69345ff25eaedc1157148d02553
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-11 23:05:59 +0500

    Sec_inYear (v0.2)

--- Assignments 00 to 05 - Homework Projects/01 - Expressions/06_seconds_in_years.py
+++ Assignments 00 to 05 - Homework Projects/01 - Expressions/06_seconds_in_years.py
@@ -3,7 +3,28 @@ print("  01 - EXPRESSIONS ")
 print("  Seconds in Year   ")
 print("-- -- -- -- -- -- --\n")
 
+usr_yrs = int(input("Enter number of years: "))
 
+# We have
+D = 365 # days
+H = 24 # hours
+M = 60 # minutes
+S = 60 # seconds
+# as constant values
+
+days = usr_yrs * D
+hours = days * H
+minutes = hours * M
+seconds = minutes * S
+
+sec_yrs = D * H * M * S
+
+print(f"""
+We have {sec_yrs} in 1 year.
+
+In {usr_yrs} year(s) we have,
+{days} days, {hours} hours, {minutes} minutes, {seconds} seconds.
+""")
 
 footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
 print(footer_A)
