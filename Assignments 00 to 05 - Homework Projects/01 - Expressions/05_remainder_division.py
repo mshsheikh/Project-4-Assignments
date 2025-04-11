e110e890d73ea21b68880acfb04e3884b12a4497
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-11 22:41:55 +0500

    Remainder Division (v0.2)

--- Assignments 00 to 05 - Homework Projects/01 - Expressions/05_remainder_division.py
+++ Assignments 00 to 05 - Homework Projects/01 - Expressions/05_remainder_division.py
@@ -3,7 +3,14 @@ print("    01 - EXPRESSIONS ")
 print("  Remainder  Division   ")
 print("-- -- -- -- -- -- -- --\n")
 
+user_num1 = int(input("Enter a number to be divided: "))
+user_num2 = int(input("Enter an number to divide by: "))
 
+Q = user_num1 // user_num2
+R = user_num1 % user_num2
+
+print(f"""\nResults:
+  Division is {Q} with a remainder of {R}.""")
 
 footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
 print(footer_A)
