c86d0d49b69a118b46c58e19b2b7989a288ad12c
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-12 21:21:03 +0500

    TML (v0.2)

--- Assignments 00 to 05 - Homework Projects/01 - Expressions/07_tiny_mad_lib.py
+++ Assignments 00 to 05 - Homework Projects/01 - Expressions/07_tiny_mad_lib.py
@@ -3,7 +3,7 @@ print("  01 - EXPRESSIONS ")
 print("   Tiny Mad Libs     ")
 print("-- -- -- -- -- -- --\n")
 
-jumla = "Roads in Karachi  fun. I saw how people take wrong ways on the road to take"
+jumla = "Roads in Karachi a fun. I saw how people take wrong ways on the road to take"
 
 adj = input("Enter an adjective: ")
 noun = input("Enter a noun: ")
