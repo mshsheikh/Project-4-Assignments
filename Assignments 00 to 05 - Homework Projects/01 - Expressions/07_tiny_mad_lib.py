fc565d5616148bc6feeaa405e6f25b62cedb9241
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-12 21:19:11 +0500

    TML (v0.2)

--- Assignments 00 to 05 - Homework Projects/01 - Expressions/07_tiny_mad_lib.py
+++ Assignments 00 to 05 - Homework Projects/01 - Expressions/07_tiny_mad_lib.py
@@ -3,7 +3,7 @@ print("  01 - EXPRESSIONS ")
 print("   Tiny Mad Libs     ")
 print("-- -- -- -- -- -- --\n")
 
-jumla = "Code in Karachi is fun. I saw how people take wrong ways on the road to take"
+jumla = "Cod in Karachi is fun. I saw how people take wrong ways on the road to take"
 
 adj = input("Enter an adjective: ")
 noun = input("Enter a noun: ")
