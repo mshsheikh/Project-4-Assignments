9da6ab2de210602113fc8c292e08e466c94ac9cc
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-15 19:39:31 +0500

    AMN (v0.2)

--- Assignments 00 to 05 - Homework Projects/02 - List/01_add_many_number.py
+++ Assignments 00 to 05 - Homework Projects/02 - List/01_add_many_number.py
@@ -3,7 +3,14 @@ print("     02 - LIST        ")
 print("  Add Many Numbers    ")
 print("-- -- -- -- -- -- --\n")
 
+numList = [2, 4, 6, 8]
 
+total = 0
+
+for number in numList:
+    total += number
+
+print(f"Total: {total}")
 
 footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
 print(footer_A)
