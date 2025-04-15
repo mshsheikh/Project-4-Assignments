7cbc460aaa851be21968322196b0129b9b1a65a4
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-15 19:40:21 +0500

    Double List (v0.2)

--- Assignments 00 to 05 - Homework Projects/02 - List/02_double_list.py
+++ Assignments 00 to 05 - Homework Projects/02 - List/02_double_list.py
@@ -3,7 +3,12 @@ print("     02 - LIST        ")
 print("    Double List       ")
 print("-- -- -- -- -- -- --\n")
 
+numList = [1, 2, 3, 4]
 
+for X in range(len(numList)):
+    numList[X] = numList[X] * 2
+
+print("List 2X:", numList)
 
 footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
 print(footer_A)
