35a015b634b29e11f6bec0cfedd53c8bacc1b205
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-15 18:19:10 +0500

    fds (v0.1)

---
+++ Assignments 00 to 05 - Homework Projects/02 - List/04_flowing_with_data_structures.py
@@ -0,0 +1,9 @@
+print("\n-- -- -- -- -- -- -- -- -- -- --")
+print("          02 - LIST               ")
+print("  Flowing with Data Structures    ")
+print("-- -- -- -- -- -- -- -- -- -- --\n")
+
+
+
+footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
