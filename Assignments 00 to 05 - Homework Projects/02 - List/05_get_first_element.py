e8990b2435635ba578aba9f7c866f5af441bd5c5
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-15 20:05:34 +0500

    gfe (v0.2)

--- Assignments 00 to 05 - Homework Projects/02 - List/05_get_first_element.py
+++ Assignments 00 to 05 - Homework Projects/02 - List/05_get_first_element.py
@@ -3,7 +3,17 @@ print("           02 - LIST              ")
 print("       Get First Element          ")
 print("-- -- -- -- -- -- -- -- -- -- --\n")
 
+num: int = int(input("""Computer: How many elements do you want to add to the list?
+                     
+User: """))
 
+user_list: list[str] = []
+
+for X in range(num):
+    user_item = input(f"\nEnter element {X + 1}: ")
+    user_list.append(user_item)
+
+print(f"\nElement 1 in the list: {user_list[0]}")
 
 footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
 print(footer_A)
