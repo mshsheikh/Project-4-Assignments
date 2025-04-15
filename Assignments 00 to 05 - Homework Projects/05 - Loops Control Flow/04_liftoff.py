f115fbbbb4778bce4fe029f866c3e9604dff143e
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-16 01:41:56 +0500

    Liftoff (v0.1)

---
+++ Assignments 00 to 05 - Homework Projects/05 - Loops Control Flow/04_liftoff.py
@@ -0,0 +1,13 @@
+print("\n-- -- -- -- -- -- -- -- -- -- --")
+print("   05 - LOOPS & CONTROL FLOW      ")
+print("            Liftoff!              ")
+print("-- -- -- -- -- -- -- -- -- -- --\n")
+
+count = 10
+while count > 0:
+    print(count, end=' ')
+    count -= 1
+print("Liftoff!")
+
+footer_A: str = "\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
