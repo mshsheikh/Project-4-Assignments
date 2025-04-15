7ec46b69b2d7e38cdb41a4f39e2abc493e1db40c
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-15 20:06:06 +0500

    gle (v0.2)

--- Assignments 00 to 05 - Homework Projects/02 - List/06_get_last_element.py
+++ Assignments 00 to 05 - Homework Projects/02 - List/06_get_last_element.py
@@ -1,9 +1,19 @@
 print("\n-- -- -- -- -- -- -- -- -- -- --")
 print("           02 - LIST              ")
-print("       Get Last Element          ")
+print("        Get Last Element          ")
 print("-- -- -- -- -- -- -- -- -- -- --\n")
 
+num: int = int(input("""Computer: How many elements do you want to add to the list?
+                     
+User: """))
 
+user_list: list[str] = []
+
+for X in range(num):
+    user_item = input(f"\nEnter element {X + 1}: ")
+    user_list.append(user_item)
+
+print(f"\nElement {num} in the list: {user_list[-1]}")
 
 footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
 print(footer_A)
