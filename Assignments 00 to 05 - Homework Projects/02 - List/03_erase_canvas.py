56393694624a9839bb429a5c388adf3d9ca55e89
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-15 18:18:30 +0500

    ec (v0.1)

---
+++ Assignments 00 to 05 - Homework Projects/02 - List/03_erase_canvas.py
@@ -0,0 +1,9 @@
+print("\n-- -- -- -- -- -- --")
+print("     02 - LIST        ")
+print("   Erase  Canvas      ")
+print("-- -- -- -- -- -- --\n")
+
+
+
+footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
