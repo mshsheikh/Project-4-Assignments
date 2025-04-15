68a4281c956cc86a3df541f0cf010c8291e55395
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-15 20:49:26 +0500

    print evens (v0.1)

---
+++ Assignments 00 to 05 - Homework Projects/03 - If Statements/01_print_evens.py
@@ -0,0 +1,11 @@
+print("\n-- -- -- -- -- -- -- -- -- -- --")
+print("       03 - FOR LOOP BASICS      ")
+print("          Printing Evens      ")
+print("-- -- -- -- -- -- -- -- -- -- --\n")
+
+for X in range(20):
+    even_nums = X * 2
+    print(even_nums, end=" ")
+
+footer_A: str = "\n\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
