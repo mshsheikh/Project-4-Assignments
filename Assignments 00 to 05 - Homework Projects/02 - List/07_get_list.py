fc32f0f422fbb0a722ffcc1100975636af5e17b4
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-15 20:06:38 +0500

    Get LIst (v0.2)

--- Assignments 00 to 05 - Homework Projects/02 - List/07_get_list.py
+++ Assignments 00 to 05 - Homework Projects/02 - List/07_get_list.py
@@ -3,7 +3,16 @@ print("     02 - LIST        ")
 print("     Get  List       ")
 print("-- -- -- -- -- -- --\n")
 
+user_list: list[str] = []
 
+while True:
+    item = input("Enter a value (or press Enter to finish): ")
+    if item == "":
+        break
+    user_list.append(item)
+
+print("\nHere’s the final list:")
+print(user_list)
 
 footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
 print(footer_A)
