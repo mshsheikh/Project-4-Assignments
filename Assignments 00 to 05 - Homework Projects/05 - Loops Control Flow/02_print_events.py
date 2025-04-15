5cb5d827a89839998aba978b273a94732b2ea40e
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-16 01:20:13 +0500

    Print Evens (v0.2)

---
+++ Assignments 00 to 05 - Homework Projects/05 - Loops Control Flow/02_print_events.py
@@ -0,0 +1,16 @@
+print("\n-- -- -- -- -- -- -- -- -- -- --")
+print("   05 - LOOPS & CONTROL FLOW      ")
+print("          Print Evens            ")
+print("-- -- -- -- -- -- -- -- -- -- --\n")
+
+count = 0
+number = 0
+
+while count < 20:
+    print(number, end=' ')
+    number += 2
+    count += 1
+
+print("\n")
+footer_A: str = "\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
