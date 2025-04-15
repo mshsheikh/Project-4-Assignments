0d051b5183fef02fb734ec311173201e8f3b099e
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-15 19:41:43 +0500

    fds (v0.2)

--- Assignments 00 to 05 - Homework Projects/02 - List/04_flowing_with_data_structures.py
+++ Assignments 00 to 05 - Homework Projects/02 - List/04_flowing_with_data_structures.py
@@ -3,7 +3,15 @@ print("          02 - LIST               ")
 print("  Flowing with Data Structures    ")
 print("-- -- -- -- -- -- -- -- -- -- --\n")
 
+message: str = input("Enter a message to copy: ")
 
+messages: list[str] = ["Empty"]
+
+print("\nList before:", messages)
+
+messages.append(message)
+
+print("List after:", messages)
 
 footer_A : str = "\nCoded by Muhammad Salman Hussain\n"
 print(footer_A)
