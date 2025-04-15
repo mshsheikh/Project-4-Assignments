ca91ee3373321d44bd1136ea0ca83ceef28022d7
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-15 20:57:05 +0500

    IVA (v0.1)

---
+++ Assignments 00 to 05 - Homework Projects/03 - If Statements/02_international_voting_age.py
@@ -0,0 +1,28 @@
+print("\n-- -- -- -- -- -- -- -- -- --")
+print("      04 - IF STATEMENTS       ")
+print("   International Voting Age    ")
+print("-- -- -- -- -- -- -- -- -- --\n")
+
+Peturksbouipo_Age = 16
+Stanlau_Age = 25
+Mayengua_Age = 48
+
+user_age = int(input("How old are you? "))
+
+if user_age >= Peturksbouipo_Age:
+    print(f"You can vote in Peturksbouipo where the voting age is {Peturksbouipo_Age}.")
+else:
+    print(f"You cannot vote in Peturksbouipo where the voting age is {Peturksbouipo_Age}.")
+
+if user_age >= Stanlau_Age:
+    print(f"You can vote in Stanlau where the voting age is {Stanlau_Age}.")
+else:
+    print(f"You cannot vote in Stanlau where the voting age is {Stanlau_Age}.")
+
+if user_age >= Mayengua_Age:
+    print(f"You can vote in Mayengua where the voting age is {Mayengua_Age}.")
+else:
+    print(f"You cannot vote in Mayengua where the voting age is {Mayengua_Age}.")
+
+footer_A: str = "\n\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
