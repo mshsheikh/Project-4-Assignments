ea53810988664bdfd25a051ea8713efb0f1fa742
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-04-16 01:42:33 +0500

    Double It (v0.2)

---
+++ Assignments 00 to 05 - Homework Projects/05 - Loops Control Flow/05_double_it.py
@@ -0,0 +1,13 @@
+print("\n-- -- -- -- -- -- -- -- -- -- --")
+print("   05 - LOOPS & CONTROL FLOW      ")
+print("           Double It!             ")
+print("-- -- -- -- -- -- -- -- -- -- --\n")
+
+value = int(input("Enter a number: "))
+
+while value < 24000:
+    value = value * 2
+    print(value, end=' ')
+
+footer_A: str = "\nCoded by Muhammad Salman Hussain\n"
+print(footer_A)
